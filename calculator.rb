20502ca3b05da7f614b8660361fa14fe1523857d
ttrladmn-byte <ttrladmn@gmail.com>
2026-07-22 16:40:07 +0800

    Add sample file

---
+++ calculator.rb
@@ -0,0 +1,7 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int firstNumber = 2;
+        int secondNumber = 3;
+        System.out.println(firstNumber + secondNumber);
+    }
+}
